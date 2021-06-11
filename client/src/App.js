a59d189b437215995a84a30213ac2fa79be24719
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-06-11 15:38:59 -0700

    Adding route for Sign-Up page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Route, BrowserRouter} from 'react-router-dom';
 import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Route exact path="/" component={Login}/>
+        <Route path="/sign-up" component={SignUp}/>
       </BrowserRouter>
     </div>
   );
