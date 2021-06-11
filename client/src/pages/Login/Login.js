cceddd9407219377fb79bcb6c0a34153d6193fc5
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-06-11 15:37:38 -0700

    Add surounding div and styling for background image, and adding classNames for Bootstrap styling on container

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -28,35 +28,36 @@ const Login = () => {
     }
 
     return (
-        <Container>
-            <Row className="mt-5" lg={2}>
-                <Image src={logo} className="mx-auto"/>
-            </Row>
-            <Row className="mt-3 mb-4">
-                <div>Connect with peers working to transform themselves through fitness</div>
-            </Row>
-            <Row lg={3}>
-                <Col lg={{ offset: 4 }}>
-                    <Form>
-                        <Form.Row className="mt-2">
-                            <Col>
-                                <Form.Control type="email" placeholder="Email" onChange={handleEmail}/>
-                            </Col>
-                        </Form.Row>
-                        <Form.Group as={Row} className="mt-4">
-                            <Col>
-                                <Form.Control type="password" placeholder="Password" onChange={handlePassword} />
-                            </Col>
-                        </Form.Group>
-                        <Form.Row>
-                                <Button variant="danger" type="submit" className="mx-4 mt-3 w-25" onClick={handleLogin}>Login</Button>
-                                <Button variant="danger" type="submit" className="mx-4 mt-3 w-25">Sign Up</Button>
-                        </Form.Row>
-                    </Form>
-                </Col>
-            </Row>
-        </Container>
-
+        <div id="main" className="d-flex align-items-center">
+            <Container className="bg-light w-75 pb-5 mt-5 rounded">
+                <Row lg={2}>
+                    <Image src={logo} className="mx-auto"/>
+                </Row>
+                <Row className="mt-3 mb-4">
+                    <div className="text-center">Connect with peers working to transform themselves through fitness</div>
+                </Row>
+                <Row lg={3}>
+                    <Col lg={{ offset: 4 }}>
+                        <Form>
+                            <Form.Row className="mt-2">
+                                <Col>
+                                    <Form.Control type="email" placeholder="Email" onChange={handleEmail}/>
+                                </Col>
+                            </Form.Row>
+                            <Form.Group as={Row} className="mt-4">
+                                <Col>
+                                    <Form.Control type="password" placeholder="Password" onChange={handlePassword} />
+                                </Col>
+                            </Form.Group>
+                            <Form.Row>
+                                    <Button variant="danger" type="submit" className="mx-3 mt-3 px-4" onClick={handleLogin}>Login</Button>
+                                    <Button href="/sign-up" variant="danger" type="submit" className="mx-3 mt-3 px-4">Sign Up</Button>
+                            </Form.Row>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     )
 }
 export default Login;
