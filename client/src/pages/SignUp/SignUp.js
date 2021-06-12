48d27aab1550e2d573bcd01a1b86284a856a16ec
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-06-11 19:07:48 -0700

    Adding state and functions to handle state and submit, then send user info to backend

--- client/src/pages/SignUp/SignUp.js
+++ client/src/pages/SignUp/SignUp.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Form, Button, Col, Row, Container} from 'react-bootstrap';
+import axios from 'axios';
 
 const SignUp = () => {
+    const [validated, setValidated] = useState(false);
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confPassword, setConfPassword] = useState('');
+
+    const handleFirstName = (event) => setFirstName(event.target.value)
+    const handleLastName = (event) => setLastName(event.target.value)
+    const handleEmail = (event) => setEmail(event.target.value)
+    const handlePassword = (event) => setPassword(event.target.value)
+    const handleConfPassword = (event) => setConfPassword(event.target.value)
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        const data = {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password,
+            confPassword: confPassword
+        }
+
+        axios.post('http://localhost:5000/sign-up', data)
+            .then(result => console.log(result.data))
+        setValidated(true);
+    }
+
+
     return (
         <div id="main" className="d-flex align-items-center">
             <Container className="bg-light w-75 pb-5 mt-5 rounded">
@@ -10,35 +46,50 @@ const SignUp = () => {
                 </Row>
                 <Row lg={2}>
                     <Col lg={{ offset: 3 }} className="text-left">
-                        <Form>
+                        <Form noValidate validated={validated} onSubmit={handleSubmit}>
                             <Form.Group as={Row} className="mt-5">
                                 <Form.Label column lg={4} className="d-flex justify-content-left">First Name:</Form.Label>
                                 <Col>
-                                    <Form.Control type="email"/>
+                                    <Form.Control required type="text" onChange={handleFirstName}/>
+                                    <Form.Control.Feedback type="invalid">
+                                        First Name is required.
+                                    </Form.Control.Feedback>
                                 </Col>
                             </Form.Group>
                             <Form.Group as={Row} className="mt-2">
                                 <Form.Label column lg={4} className="d-flex justify-content-left">Last Name:</Form.Label>
                                 <Col>
-                                    <Form.Control type="email"/>
+                                    <Form.Control required type="text" onChange={handleLastName}/>
+                                    <Form.Control.Feedback type="invalid">
+                                        Last Name is required.
+                                    </Form.Control.Feedback>
                                 </Col>
                             </Form.Group>
                             <Form.Group as={Row} className="mt-2">
                                 <Form.Label column lg={4} className="d-flex justify-content-left">Email:</Form.Label>
                                 <Col>
-                                    <Form.Control type="email"/>
+                                    <Form.Control required type="email" onChange={handleEmail}/>
+                                    <Form.Control.Feedback type="invalid">
+                                        Email is required.
+                                    </Form.Control.Feedback>
                                 </Col>
                             </Form.Group>
                             <Form.Group as={Row} className="mt-2">
                                 <Form.Label column lg={4} className="d-flex justify-content-left">Password:</Form.Label>
                                 <Col>
-                                    <Form.Control type="password"/>
+                                    <Form.Control required type="password" onChange={handlePassword}/>
+                                    <Form.Control.Feedback type="invalid">
+                                        Password is required.
+                                    </Form.Control.Feedback>
                                 </Col>
                             </Form.Group>
                             <Form.Group as={Row} className="mt-2">
                                 <Form.Label column lg={4} className="d-flex justify-content-left">Confirm Password:</Form.Label>
                                 <Col>
-                                    <Form.Control type="password"/>
+                                    <Form.Control required type="password" onChange={handleConfPassword}/>
+                                    <Form.Control.Feedback type="invalid">
+                                        Password confirmation is required.
+                                    </Form.Control.Feedback>
                                 </Col>
                             </Form.Group>
                             <Form.Group as={Row}>
