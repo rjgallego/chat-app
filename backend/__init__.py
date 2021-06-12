06fa2773d6222e6fa2a080af67a58e48374c7e58
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-06-11 19:13:23 -0700

    Adding environment variables and SQLAlchemy configurations for database

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,9 +1,18 @@
 from flask import Flask
 from flask_cors import CORS
 
+from dotenv import load_dotenv
+import os
+from os.path import join, dirname
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'fnfduafniwalwieuh'
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('POSTGRES_URI')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     CORS(app)
 
     from .views import views
