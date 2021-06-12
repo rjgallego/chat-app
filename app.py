d0d87c40afd73f17f8515587086ce493ce0a15b1
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-06-11 19:06:15 -0700

    adding environment variables and db setup

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from backend import create_app
+from routes import create_app
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+import os
 
+load_dotenv()
 app = create_app()
+db = SQLAlchemy(app)
+
+if os.environ.get('ENV') == 'dev':
+    app.debug = True
+else:
+    app.debug = False
 
 if __name__ == '__main__':
-    app.debug = True
     app.run()
-
