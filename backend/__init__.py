4464880f3c2c0033cb5f67840298a3101a67c815
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-06-14 14:52:00 -0700

    moving everything outside of function so app and db can be imported separately

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 from dotenv import load_dotenv
 import os
@@ -8,17 +9,16 @@ from os.path import join, dirname
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('POSTGRES_URI')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    CORS(app)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('POSTGRES_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+CORS(app)
 
-    from .views import views
-    from .auth import auth
+db = SQLAlchemy(app)
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+from .views import views
+from .auth import auth
 
-    return app
+app.register_blueprint(views, url_prefix='/')
+app.register_blueprint(auth, url_prefix='/')
