d4161064441a42f37c5a529964b5bb5ae4a659a9
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-06-14 20:15:30 -0700

    Adding route for Home page

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import { Route, BrowserRouter} from 'react-router-dom';
 import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Route exact path="/" component={Login}/>
+        <Route exact path="/" component={Home}/>
         <Route path="/sign-up" component={SignUp}/>
+        <Route path="/login" component={Login}/>
       </BrowserRouter>
     </div>
   );
