2746578ea4e9faff20a681624ddc72414195b703
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-06-16 18:28:34 -0700

    Adding date to model constructor

--- backend/models.py
+++ backend/models.py
@@ -27,3 +27,20 @@ class ChannelModel(db.Model):
 
     def __repr__(self):
         return '<name {}>'.format(self.name)
+
+class MessageModel(db.Model):
+    __tablename__ = 'messages'
+    id = db.Column(db.Integer, primary_key=True)
+    channel_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
+    text = db.Column(db.String(1000))
+    date = db.Column(db.DateTime)
+
+    def __init__(self, channel_id, user_id, text, date):
+        self.channel_id = channel_id
+        self.user_id = user_id
+        self.text = text
+        self.date = date
+    
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
