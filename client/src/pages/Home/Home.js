07df3229e56909c3e70603f502fb3dcd22d502c0
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-06-16 17:10:56 -0700

    Adding selectedChannel state for keeping state of current selected channel

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import ChannelCard from './ChannelCard'
-import {Container, Row, Col, Nav, Card} from 'react-bootstrap'
+import MessageBoard from './MessageBoard'
+import {Container, Row, Col, Nav} from 'react-bootstrap'
 import {Redirect} from 'react-router-dom'
 
 const Home = () => {
+    const [selectedChannel, setSelectedChannel] = useState("")
+
+    const handleClick = () => sessionStorage.removeItem('token')
+
     if(!sessionStorage.getItem("token")){
         return <Redirect to="/login" />
     }
@@ -13,39 +18,16 @@ const Home = () => {
             <Row>
                 <Nav className="bg-danger border border-light">
                     <Nav.Item>
-                        <Nav.Link href="/login" className="text-light">Logout</Nav.Link>
+                        <Nav.Link href="/login" className="text-light" onClick={handleClick}>Logout</Nav.Link>
                     </Nav.Item>
                 </Nav>
             </Row>
             <Row>
                 <Col>
-                    <ChannelCard />
+                    <ChannelCard selected={selectedChannel} setSelected={setSelectedChannel} />
                 </Col>
                 <Col lg={10}>
-                    <Card className="bg-light w-100 mt-3 p-1" style={{height: '85vh'}}>
-                        <div>
-                            <Card className="w-50 p-0 m-0 float-right bg-warning" style={{fontSize: '0.75rem', float: 'right'}}>
-                                <Card.Body>
-                                    <Card.Link className="text-dark text-decoration-none" style={{fontSize: '0.9rem', fontWeight:'bold'}}>Joe Schmoe</Card.Link>
-                                    <Card.Link className="text-muted text-decoration-none">06-14-21 8:30pm</Card.Link>
-                                    <Card.Text className="p-0 mt-2">
-                                        Some quick example text to build on the card title and make up the bulk of
-                                        the card's content.
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </div>
-                        <Card className="w-50 p-0 m-0"  style={{fontSize: '0.75rem'}}>
-                            <Card.Body>
-                                <Card.Link className="text-dark text-decoration-none" style={{fontSize: '0.9rem', fontWeight:'bold'}}>Don Draper</Card.Link>
-                                <Card.Link className="text-muted text-decoration-none">06-14-21 8:30pm</Card.Link>
-                                <Card.Text className="p-0 mt-2">
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Card>
+                    <MessageBoard />
                 </Col>
             </Row>
         </Container>
