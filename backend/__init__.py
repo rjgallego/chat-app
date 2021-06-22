56785fc4039870506ce44d9a3948f3bca5d7afa9
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-06-22 16:17:54 -0700

    adding prefix to auth path and adding route to serve index.html file

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory,make_response
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
@@ -10,7 +10,7 @@ from os.path import join, dirname
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder='../client/build',static_url_path='/')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('POSTGRES_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -26,4 +26,8 @@ from .models import UserModel
 jwt = JWTManager(app)
 
 app.register_blueprint(views, url_prefix='/')
-app.register_blueprint(auth, url_prefix='/')
+app.register_blueprint(auth, url_prefix='/auth')
+
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
